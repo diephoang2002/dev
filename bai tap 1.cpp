56175c08a69381c605fe2c11b11344598eb79e58
diephoang2002 <71093476+diephoang2002@users.noreply.github.com>
2020-09-15 08:11:16 +0700

    bai lan 2

---
+++ bai tap 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+	int a=2,b=8;
+	printf ("nhap a=");
+	scanf ("%d",&a);
+	
+	int b
+	printf ("nhap b=");
+    scanf ("%d",&b);
+    
+     if(a,b){
+     	printf("day la thu 2");
+    }else{
+    	printf("day la chu nhat");
+}
---
+++ bai tap2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+		int a,b;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+	printf("nhap b=");
+	scanf("%d",&b);
+	
+	  if(a>b){
+	  	printf("")
+	  }
+    	
+
+	
+
